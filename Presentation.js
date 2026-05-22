33cc89d77b1b17ead062399692d5385b48a289b6
halimgambit <70286759+halimgambit@users.noreply.github.com>
2026-05-22 20:24:59 +0200

    Initial commit

---
+++ Presentation.js
@@ -0,0 +1,33 @@
+export async function init () {
+    await Avatar.lang.addPluginPak('Presentation');
+}
+
+export async function action(data, callback) {
+
+    try {
+
+        const Locale = await Avatar.lang.getPak('Presentation', data.language);
+
+        const tblActions = {
+            sePresente: () => sePresente(data.client, Locale)
+        };
+
+        info("Presentation:", data.action.command, Locale.get("plugin.from"), data.client);
+
+        if (tblActions[data.action.command]) {
+            await tblActions[data.action.command]();
+        }
+
+    } catch (err) {
+        if (data.client) {Avatar.Speech.end(data.client);}
+        if (err.message) {error(err.message);
+        }
+    }
+
+    callback();
+}
+
+const sePresente = (client, Locale) => {
+    Avatar.speak(Locale.get('speech.answerSet'), client);
+};
+
